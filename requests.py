234b17a75d7420f4388531415c94f2b72e889666
rockrashit19 <rd.galiullin@gmail.com>
2025-07-19 19:37:26 +0300

    first commit

---
+++ requests.py
@@ -0,0 +1,43 @@
+from sqlalchemy import select, update, delete, func
+from models import async_session, User, Task
+from pydantic import BaseModel, ConfigDict
+from typing import List
+
+
+class TaskSchema(BaseModel):
+    id: int
+    title: str
+    completed: bool
+    user: int
+    
+    model_config = ConfigDict(from_attributes=True)
+
+
+async def add_user(tg_id):
+    async with async_session() as sessions:
+        user = await sessions.scalar(select(User).where(User.tg_id == tg_id))
+        if user:
+            return user
+        
+        new_user = User(tg_id=tg_id)
+        sessions.add(new_user)
+        await sessions.commit()
+        await sessions.refresh(new_user)
+        return new_user
+    
+    
+async def get_tasks(user_id):
+    async with async_session() as session:
+        tasks = await session.scalars(
+            select(Task).where(Task.user == user_id, Task.completed == False)
+        )
+        
+        serialized_tasks = [
+            TaskSchema.model_validate(t).model_dump() for t in tasks
+        ]
+        
+        return serialized_tasks
+    
+async def get_completed_task_count(user_id):
+    async with async_session() as session:
+        return await session.scalar(select(func.count(Task.id)).where(Task.completed == True))
